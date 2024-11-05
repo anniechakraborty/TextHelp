3ea216496dca9e0146d607821d82a7211f17364e
Annie Chakraborty <46227364+anniechakraborty@users.noreply.github.com>
2024-11-05 09:26:21 +0100

    text font integrated

--- App.tsx
+++ App.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
-import { SafeAreaView, StyleSheet } from 'react-native';
+import { SafeAreaView, StyleSheet, View, Text } from 'react-native';
 import TextInputComponent from './components/TextInputComponent';
 
 const App = () => {
   return (
     <SafeAreaView style={styles.container}>
+      <Text style={styles.regularText}>
+        Hello World
+      </Text>
       <TextInputComponent />
     </SafeAreaView>
   );
@@ -16,6 +19,14 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'center',
     backgroundColor: '#ffffff',
+  },
+  regularText: {
+    fontFamily: 'SortsMillGoudy-Regular',
+    fontSize: 36,
+  },
+  italicText: {
+    fontFamily: 'SortsMillGoudy-Italic',
+    fontSize: 20,
   },
 });
 
